6adf95a3bcb62d8edf6dd09a5e08ce6f43489650
Tuhinanshu Modgil <tuhinanshumodgil18@gmail.com>
2024-07-16 22:56:20 +0530

    added mongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import {DB_NAME} from '../constants'
+
+const connectDB = async ()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_CONNECTION}/${DB_NAME}`)
+        console.log("Database connected with connection instance: ", connectionInstance)
+    }
+    catch(error){
+        console.log("Database connection error: ", error.message)
+    }
+}
+
+export default connectDB
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import { app } from "./app.js";
 import 'dotenv/config'
+import connectDB from "./db/index.js";
 
 // dotenv.config({
 //     path: './env'
 // })
 
-app.listen(process.env.PORT||5000, ()=>{
-    console.log(`server running at ${process.env.PORT||5000}`)
+
+connectDB()
+.then(()=>{
+    app.on("error", (err)=>{
+        console.log("Error after database connection: ", err.message)
+        throw err
+    })
+}
+
+)
+.then(()=>{
+    console.log("Starting server listening")
+    app.listen(process.env.PORT||5000, ()=>{
+        console.log(`server running at ${process.env.PORT||5000}`)
+    })
 })
+.catch((err)=>{
+    console.log("failed to start server: ", err.message)
+})
+
